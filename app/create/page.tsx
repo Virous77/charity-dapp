8b8df96b95822b820ec1b72aa6a106f5849786e5
Reetesh Kumar <rajreetesh7@gmail.com>
2024-02-21 22:35:15 +0530

    completed first version

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,4 +1,18 @@
 import Create from "@/components/charity/charity-create/create";
+import { commonMetaData } from "@/utils/utils";
+
+export async function generateMetadata() {
+  const metaData = commonMetaData({
+    name: "Create | Charity",
+    desc: "Always be giving back to the community. Charity is key. Charity makes it easy for you to get started and be full transparent with your donors. Wagmi",
+    image:
+      "https://res.cloudinary.com/dw6wav4jg/image/upload/v1708533691/android-chrome-512x512_lmq9mv.png",
+    url: "/create",
+  });
+  return {
+    ...metaData,
+  };
+}
 
 const CharityCreate = () => {
   return (
---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+export default async function sitemap() {
+  const URL = "https://charity-next.vercel.app";
+
+  const restUrls = [
+    {
+      url: `${URL}/`,
+      lastModified: new Date(),
+      priority: 1.0,
+      changeFrequency: "weekly",
+    },
+    {
+      url: `${URL}/create`,
+      lastModified: new Date(),
+      priority: 0.9,
+      changeFrequency: "daily",
+    },
+    {
+      url: `${URL}/charity/1/Donate-funds-to-Childrens-always`,
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: "weekly",
+    },
+  ];
+
+  return [...restUrls];
+}
