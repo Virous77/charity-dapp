73109daca902aea9f8825d506519dc0d414b931f
Reetesh Kumar <rajreetesh7@gmail.com>
2024-02-22 22:28:20 +0530

    updated cache settings and UI style issue fix

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,10 +1,13 @@
+"use client";
+
 import ThemeSwitcher from "@/lib/theme";
 import ConnectButton from "@/lib/wagmi/wallet-modal";
 import { BadgePlus } from "lucide-react";
 import Link from "next/link";
-import { initialState } from "@/app/layout";
+import { useAccount } from "wagmi";
+
 const Navbar = () => {
-  const data = initialState();
+  const { address } = useAccount();
   return (
     <nav className=" w-[95%] md:w-[80%] fixed top-10 m-auto z-[20] py-2 px-4 rounded-[30px] bg-muted  left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-between">
       <Link href="/">
@@ -12,7 +15,7 @@ const Navbar = () => {
       </Link>
 
       <div className=" flex items-center gap-4">
-        {data?.current && (
+        {address && (
           <Link href="/create?edit=false">
             <BadgePlus size={20} />
           </Link>
